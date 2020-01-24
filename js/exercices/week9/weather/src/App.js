c253f24efc5187e28f4122860c3422c3ee75486c
Devail1 <54677701+Devail1@users.noreply.github.com>
2020-01-24 09:37:17 +0200

    stateless componenet

---
+++ js/exercices/week9/weather/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import WeatherCard from './WeatherCard';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" id="root">
+      <WeatherCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ js/exercices/week9/weather/src/WeatherCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const WeatherCard = (props) => {
+    return ( 
+         <div className="card">
+             <h1>Sydney</h1>
+             <h3>AU</h3>
+             <img src="" alt="Wather Icon"/>
+             <h1>20</h1>
+             <h3>°C</h3>
+         </div>
+     );
+}
+ 
+export default WeatherCard;
